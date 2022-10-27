6dbff77e3b297e022a8a454963662b93dcfb0b0b
prayogayogi <yogikpiter@gmail.com>
2022-10-27 22:43:53 +0700

    add isi function

--- controllers/ordercontroller/ordercontroller.go
+++ controllers/ordercontroller/ordercontroller.go
@@ -2,11 +2,12 @@ package ordercontroller
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/prayogayogi/go-restapi-gin/models"
 )
 func Index(c *gin.Context){
-	var orders models.Order
+	var orders []models.Order
 
 	models.DB.Find(&orders)
 	c.JSON(http.StatusOK, orders)
@@ -14,20 +15,39 @@ func Index(c *gin.Context){
 
 func Create(c *gin.Context){
 	var order models.Order
-
 	if err := c.ShouldBindJSON(&order); err != nil{
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
-
-		models.DB.Create(&order)
-		c.JSON(http.StatusOK, gin.H{"order":order})
 	}
+	models.DB.Create(&order)
+	c.JSON(http.StatusOK, gin.H{"order": order})
 }
 
 func Update(c *gin.Context){
+	var order models.Order
+	id := c.Param("orderid")
 
+	if err := c.ShouldBindJSON(&order); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	if models.DB.Model(&order).Where("order_id = ?", id).Updates(&order).RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "tidak dapat mengupdate order"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Data berhasil diperbarui"})
 }
 
 func Delete(c *gin.Context){
+	var order models.Order
+	id := c.Param("orderid")
 
+	if models.DB.Model(&order).Where("order_id = ?", id).Delete(&order).RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Tidak dapat di hapus"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Data berhasil dihapus"})
 }
